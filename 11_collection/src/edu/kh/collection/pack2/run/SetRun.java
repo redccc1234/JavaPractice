f4d324558e3aa89146780ed02cae727da3241bd9
redccc1234 <redccc1234@gmail.com>
2025-10-02 12:36:40 +0900

    자바연습

---
+++ 11_collection/src/edu/kh/collection/pack2/run/SetRun.java
@@ -0,0 +1,15 @@
+package edu.kh.collection.pack2.run;
+
+import edu.kh.collection.pack2.model.service.SetService;
+
+public class SetRun {
+
+	public static void main(String[] args) {
+		
+		SetService ss = new SetService();
+		ss.method4();
+		
+		
+	}
+
+}
